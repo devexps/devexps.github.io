3bc65676ac4ee01d7dc1f799bb2dec5eac9cc0ee
devexps <dev.experiences@gmail.com>
2023-05-29 15:20:18 +0700

    feat: add analytic code

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -51,6 +51,10 @@ const config = {
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
                 },
+                gtag: {
+                    trackingID: 'G-L24NDYGW09',
+                    anonymizeIP: true,
+                },
             }),
         ],
     ],
